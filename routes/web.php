3bf0612f56d26bd9e7397f3a3d69a57d25f22bd5
MuhammadHayat98 <muhammad.hayat.698@my.csun.edu>
2018-07-19 15:31:27 -0700

    commented out unused routes

--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,16 @@ $router->get('/', function () use ($router) {
 });
 
 //Workorder model test
-$router->get('/findWo/{Wo}', function (int $Wo) use ($router) {
-    $wo = Workorder::find($Wo);
-    return $wo->toJson();
-});
+// $router->get('/findWo/{Wo}', function (int $Wo) use ($router) {
+//     $wo = Workorder::find($Wo);
+//     return $wo->toJson();
+// });
 
-//Workrequest model test
-$router->get('/findWr/{Wr}', function (String $Wr) use ($router) {
-    $wr = WorkRequest::find($Wr);
-    return $wr->toJson();
-});
+// //Workrequest model test
+// $router->get('/findWr/{Wr}', function (String $Wr) use ($router) {
+//     $wr = WorkRequest::find($Wr);
+//     return $wr->toJson();
+// });
 
 //Workorder create test
 $router->post('/CreateWo', [
