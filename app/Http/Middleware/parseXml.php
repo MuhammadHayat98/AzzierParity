0f6e68370c8d5aaf98042dc3ec7ae3d16c3ff430
MuhammadHayat98 <muhammad.hayat.698@my.csun.edu>
2018-07-19 12:06:04 -0700

    hopefully fixed simple xml error

--- app/Http/Middleware/parseXml.php
+++ app/Http/Middleware/parseXml.php
@@ -16,7 +16,7 @@ class parseXml
     public function handle($request, Closure $next)
     {   
         $xmlStr = trim($request->getContent());
-        $xml = new SimpleXMLElement($xmlStr);
+        $xml = new \SimpleXMLElement($xmlStr);
         $WorkorderObj = $xml->{'WorkOrder'};
         $request->attributes->set('WorkOrder', $WorkorderObj);
         return $next($request);
