1c5962a7114b9c2822a9e537ef30e6cf46b029b0
MuhammadHayat98 <muhammad.hayat.698@my.csun.edu>
2018-07-20 11:48:14 -0700

    made boiler plate code

---
+++ app/Http/Controllers/WorkRequests.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\WorkRequest as WorkRequest;
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+
+class WorkRequests extends Controller
+{
+      /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+
+}
