5991f1119249cb98719c5be50c7c3062d32f4f18
MuhammadHayat98 <muhammad.hayat.698@my.csun.edu>
2018-07-20 12:41:25 -0700

    correct fields in work request model

--- app/WorkRequest.php
+++ app/WorkRequest.php
@@ -6,10 +6,11 @@ class WorkRequest extends Eloquent {
     protected $collection = 'WorkRequest';
     protected $primaryKey = 'WrNum';
     public $timestamps = true;
+    protected $dates = ['CreateDate', 'ModifyDate', 'Date'];
     protected $fillable = [
         'Contact', 'Phone', 'CreatedBy',
-        'CreateDate', 'Location', 'Description',
-        'WrNum'
+        'CreateDate', 'Location', 'Date', 
+        'Description','Status', 'WrNum'
     ];
     
 
