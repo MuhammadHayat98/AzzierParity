1a1d08aabf6cb47209ecb79e57fa53801d797301
MuhammadHayat98 <muhammad.hayat.698@my.csun.edu>
2018-07-20 12:42:03 -0700

    finished update and create

--- app/Http/Controllers/WorkRequests.php
+++ app/Http/Controllers/WorkRequests.php
@@ -17,5 +17,41 @@ class WorkRequests extends Controller
         //
     }
 
+    public function create(Request $request) {
+        $newWr = new WorkRequest;
+        $WrObj = $request->get('responseObj');
+        $createDateStr = (string)$WrObj->{'CreateDate'};
+        $createDateCarbon = (strlen($createDateStr) == 0) ? Carbon::now() : Carbon::createFromTimeString(substr($createDateStr,0,19), 'PST')->addHours(-1);
+        $dateStr = (string)$WrObj->{'Date'};
+        $dateCarbon = (strlen($dateStr) == 0) ? Carbon::now() : Carbon::createFromTimeString(substr($dateStr,0,19), 'PST')->addHours(-1);
+        $newWr::create([
+            'Contact' => (string)$WrObj->{'Contact'},
+            'Phone' => (string)$WrObj->{'Phone'},
+            'CreatedBy' => (string)$WrObj->{'CreatedBy'},
+            'CreateDate' => $createDateCarbon,
+            'Location' => (string)$WrObj->{'Location'},
+            'Date' => $dateCarbon,
+            'Description' => (string)$WrObj->{'Description'},
+            'Status' => (string)$WrObj->{'Status'},
+            'WrNum' => (string)$WrObj->{'WrNum'}
+        ]);
+        $newWr->save();
+    }
+
+    public function update(Request $request) {
+        $WrObj = $request->get('responseObj');
+        $modifyDateStr = (string)$WrObj->{'ModifyDate'};
+        $modifyDateCarbon = (strlen($modifyDateStr) == 0) ? Carbon::now() : Carbon::createFromTimeString(substr($modifyDateStr,0,19), 'PST')->addHours(-1);
+        $Wr = WorkRequest::find((string)$WrObj->{'WrNum'});
+        $Wr->Contact = (string)$WrObj->{'Contact'};
+        $Wr->Phone = (string)$WrObj->{'Phone'};
+        $Wr->Location = (string)$WrObj->{'Location'};
+        $Wr->Description = (string)$WrObj->{'Description'};
+        $Wr->Status = (string)$WrObj->{'Status'};
+        $Wr->ModifyBy = (string)$WrObj->{'ModifyBy'};
+        $Wr->ModifyDate = $modifyDateCarbon;
+        $Wr->save();
+    }
+
 
 }
