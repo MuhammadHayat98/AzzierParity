2cfd55a74764dbaec74b97f0d55fe0bf7229a848
HERRERAOSMAR83 <osmar.herreraperez.945@my.csun.edu>
2018-12-04 15:43:48 -0800

    debug

--- app/Http/Controllers/WorkRequests.php
+++ app/Http/Controllers/WorkRequests.php
@@ -56,7 +56,7 @@ class WorkRequests extends Controller
         $keys = array_keys($ar);
         $WrJson = (array)$ar[$keys[0]];
         $Wr = WorkRequest::where('WrNum', (string)$ar[$keys[0]]->{'WrNum'})->first();
-        Log::debug(print_r($ar) . "\n" . print_r($Wr));
+        Log::debug((string)$ar[$keys[0]]->{'WrNum'} . "\n" . $WrJson['WrNum'] );
         $Wr->update($WrJson);
         return response()->json("Update successful ", 201);
         // $modifyDateStr = (string)$ar[$keys[0]]->{'ModifyDate'};
