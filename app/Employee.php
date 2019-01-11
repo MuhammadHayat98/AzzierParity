85f6234aaab22d0a4669b935dd19d027a2a79b79
HERRERAOSMAR83 <osmar.herreraperez.945@my.csun.edu>
2019-01-11 14:25:28 -0800

    channged collection to tabele for dynamodb

--- app/Employee.php
+++ app/Employee.php
@@ -1,14 +1,14 @@
 <?php
 
 namespace App;
-use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+use \Nord\Lumen\DynamoDb\Domain\Model\DynamoDbModel as Eloquent;
 
 class Employee extends Eloquent {
-    protected $collection = 'Employee';
-    //protected $primaryKey = 'Empid';
-    public $timestamps = true;
+    protected $table = 'EmployeeTest';
+    protected $primaryKey = 'Empid';
+    //public $timestamps = true;
 
-    protected $dates = ['HireDate'];
+    //protected $dates = ['HireDate'];
     protected $fillable = [
         'Empid', 'FirstName', 'LastName', 'HireDate',
         'Craft', 'Rate', 'Location'
