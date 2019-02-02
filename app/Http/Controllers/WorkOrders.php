f521051c5d0b0c0ea728242d02a382abeb1accec
muhammadhayat98 <muhammad.hayat.698@my.csun.edu>
2019-02-01 16:12:14 -0800

    room fix

--- app/Http/Controllers/WorkOrders.php
+++ app/Http/Controllers/WorkOrders.php
@@ -38,7 +38,7 @@ class WorkOrders extends Controller
         $WoJson = (array)$ar[$keys[0]];
         //$WoJson['WoNumStr'] = (int)$WoJson['WoNumStr'];
         //$WoJson['WoNumStr'] = (string)$WoJson['WoNum'];
-        $newWo::create($WoJson);
+        $newWo::update($WoJson);
     }
 
     public function show() {
